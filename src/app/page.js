f305fc09bf2e4a3cecf3ba80d5209c376d3e9a98
portfolio-wq <spotib2@gmail.com>
2024-07-28 17:14:35 +0100

    dsd

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,25 @@
 "use client";
-import Image from "next/image";
 import { PhotoProvider, PhotoView } from "react-photo-view";
 import "react-photo-view/dist/react-photo-view.css";
 
 export default function Home() {
   const images = [
     "https://rb.gy/nw4n8l",
-    "https://rb.gy/sbfjh6",
-    "https://rb.gy/czg89r",
-    "https://source.unsplash.com/rMHNK_skwwU",
-    "https://source.unsplash.com/WBMjuGpbrCQ",
-    "https://source.unsplash.com/nCUZ5BYBL_o",
-    "https://source.unsplash.com/3u4fzMQZhjc",
-    "https://source.unsplash.com/haOIqIPSwps",
-    "https://source.unsplash.com/3UrYD7NNVxk",
-    "https://source.unsplash.com/fm1JKDItlVM",
+    "https://images.unsplash.com/photo-1722100351919-86ee668c88db?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1722082769464-58b8fc317c98?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1721404832698-4e300fa83b9b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1722077703164-b2c0cbefd9e5?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dtps://source.unsplash.com/3u4fzMQZhjc",
+    "https://images.unsplash.com/photo-1722082095682-510a3ce6d59f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1722072977408-0865d9cc3904?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1722152249965-4beb868cb004?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
   return (
     <PhotoProvider>
       <div className="px-5 mt-10 sm:p-8">
-        <div className="columns-2 gap-5 [&>img:not(:first-child)]:mt-8">
+        <div className=" columns-2 gap-5 [&>img:not(:first-child)]:mt-8">
           {images.map((item, i) => (
             <PhotoView key={i} src={item}>
-              <img src={item} />
+              <img src={item} alt="" />
             </PhotoView>
           ))}
         </div>
